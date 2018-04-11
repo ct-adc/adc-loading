1c82f16c06e37b6805c11f645d278ef509232608
马潮 <632859571@qq.com>
2018-04-11 11:33:14 +0800

    修改样式

--- src/component/main.vue
+++ src/component/main.vue
@@ -29,7 +29,8 @@
 		right: 0;
 		bottom: 0;
 		left: 0;
-		background-color: rgba(255, 255, 255, 0.8);
+		background-color: rgba(255, 255, 255, 0.9);
+		z-index: 1000000;
 	}
 	.fade-enter,.fade-leave-active{
 		opacity: 0;
@@ -45,6 +46,7 @@
 		width: 100%;
 		margin-top: -30px;
 		text-align: center;
+		z-index: 1000001;
 	}
 	.loading-icon{
 		background: url(../imgs/loading.gif) no-repeat;
